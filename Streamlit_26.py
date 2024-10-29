4884524a4a4dd211fb6b2b5c9287e6d6c2558d67
Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>
2024-10-29 14:34:41 +0100

    Update Streamlit_26.py
    Signed-off-by: Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>

--- Streamlit_26.py
+++ Streamlit_26.py
@@ -67,13 +67,13 @@ ciudades = df['city'].unique().tolist()
 df_sin_na = df.dropna()
 df_limpio = limpiezadedatos(df_sin_na)
 
-ACCESS_KEY = st.secrets["UNSPLASH_ACCESS_KEY"]
 
 def obtener_imagen_ciudad(city):
     """
     Función para obtener una imagen de una ciudad usando la API de Unsplash.
     """
-    global ACCESS_KEY
+    ACCESS_KEY = st.secrets.get("UNSPLASH_ACCESS_KEY")
+    print("ACCESS_KEY:", ACCESS_KEY)
     # Agregar "city" al término de búsqueda para especificar que queremos una imagen de la ciudad
     query = f"{city} city"
     url = f"https://api.unsplash.com/search/photos?query={query}&client_id={ACCESS_KEY}&per_page=1"
