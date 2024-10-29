5ffa4127e0ed1a97ad1fb782e675b76619e8bd4a
Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>
2024-10-29 14:07:35 +0100

    Update Streamlit_26.py
    Signed-off-by: Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>

--- Streamlit_26.py
+++ Streamlit_26.py
@@ -73,7 +73,7 @@ def obtener_imagen_ciudad(city):
     """
 
     try:
-        UNSPLASH_ACCESS_KEY = st.secrets.get("UNSPLASH_ACCESS_KEY")
+        UNSPLASH_ACCESS_KEY = "gqIKU6eUBiJ8IMpCHpgcodHhixpEMvX-yh6UVam0whxQ"
     except KeyError:
         st.error("No se encontró la clave de API de Unsplash en los secretos de Streamlit.")
         return None
