e97b5c89ef1b456b8feae570e03340a12bdfef20
Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>
2024-10-29 14:58:10 +0100

    Update connection.py
    Signed-off-by: Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>

--- code/connection.py
+++ code/connection.py
@@ -1,13 +1,15 @@
 import pymysql
+import os
 
 def get_connection():
     try:
         return pymysql.connect(
-            host ='localhost',
-            user = 'root',
-            password= 'password',
-            db='Airbnb'
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            db=os.getenv("DB_NAME")
         )
-    except Exception as ex:
-        print('Exception:{}'.format(str(ex)))
+    except Exception as e:
+        print("Error de conexión a la base de datos:", e)
+        return None
 
